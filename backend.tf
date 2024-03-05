8c5a872aa844de0563eb31c2672c09501ffb0098
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2024-03-05 12:34:14 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "Azure"
-    storage_account_name = "teststorage01"
-    container_name       = "statefile"
-    key                  = "terraform.tfstate"
-    # Add other backend configuration options as needed
-  }
-}
