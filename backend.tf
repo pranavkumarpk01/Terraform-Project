ba92589edc3009a4a0772b20e06af5b3063df84f
pranavkumarpk01 <124168954+pranavkumarpk01@users.noreply.github.com>
2024-03-05 12:20:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "new-resource"
-    storage_account_name = "mystorageaccounti"
+    resource_group_name = "Azure"
+    storage_account_name = "teststorage01"
     container_name       = "statefile"
     key                  = "terraform.tfstate"
     # Add other backend configuration options as needed
